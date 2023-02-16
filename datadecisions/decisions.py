a9922a31508884822ed00cfa17654bc8a40e71b8
xloem <0xloem@gmail.com>
2023-02-16 17:11:35 -0500

    Update decisions.py

--- datadecisions/decisions.py
+++ datadecisions/decisions.py
@@ -4,6 +4,10 @@ class TransformerDecisions:
   def __init__(self, model):
     self.model = model
   @classmethod
+  def load(cls, pathname):
+    # could also load config first, create, and use an instance method
+    return cls(Model.from_pretrained(pathname))
+  @classmethod
   def create(cls, observations, actions, rewards=1, steps=4096, embeds=128, heads=1, layers=3):
     return cls(Model(Config(
         state_dim=observations,
