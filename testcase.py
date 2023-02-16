0ed14d1a04f8da419ccd09ae9bbcc447be05de7b
xloem <0xloem@gmail.com>
2023-02-16 12:37:19 -0500

    Create testcase.py

---
+++ testcase.py
@@ -0,0 +1,4 @@
+# write here a way to reproduce the tutorial more usefully and simply
+
+# ensure it is easy to make new environments and provide custom data,
+# without needing to refer to external documentation or many different files
