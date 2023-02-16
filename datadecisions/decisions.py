2cfcd22a12af37091040ba81070bde53c96357d8
xloem <0xloem@gmail.com>
2023-02-16 17:06:27 -0500

    Update decisions.py

--- datadecisions/decisions.py
+++ datadecisions/decisions.py
@@ -1,6 +1,9 @@
+from .transformer import Config, Model
+
 class TransformerDecisions:
   def __init__(self, model):
     self.model = model
   @classmethod
   def create(cls, observations, actions, rewards=1, steps=4096, embeds=128, heads=1, layers=3):
-  
+    return cls(Model(Config(
+    )))
